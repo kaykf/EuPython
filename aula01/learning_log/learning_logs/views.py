0e03ea0f3f72b489697154633c8af1542ac29b2e
kaykf <kaykfelipe071@gmail.com>
2024-06-30 18:49:17 +0800

    views
    criei a função topics

--- aula01/learning_log/learning_logs/views.py
+++ aula01/learning_log/learning_logs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Página Principal do Learning_Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
