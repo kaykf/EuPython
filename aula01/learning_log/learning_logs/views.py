c2fd01dc3615b7867b162034dd52e8818746030e
kaykf <kaykfelipe071@gmail.com>
2024-08-03 14:00:14 -0300

    views
    add a new_topic e as possibilidades de interação

--- aula01/learning_log/learning_logs/views.py
+++ aula01/learning_log/learning_logs/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect # type: ignore
+from django.urls import reverse # type: ignore
 
 def index(request):
     """Página Principal do Learning_Log"""
@@ -17,3 +20,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adiciona um novo assunto"""
+    if request.method != 'POST':
+        #Nenhum dado submetido cria um formulario em branco
+        form = TopicForm()
+    else:
+        #Dados de post submetidos processa os dados
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #return render para voltar para a mesma pag
+            return HttpResponseRedirect(reverse('topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
