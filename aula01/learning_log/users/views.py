a3a386e897b8a690d7b331989317eb1eb7659ecb
kaykf <kaykfelipe071@gmail.com>
2024-09-22 18:19:07 -0300

    views
    importei o necessário e criei a def register

--- aula01/learning_log/users/views.py
+++ aula01/learning_log/users/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponseRedirect # type: ignore
 from django.urls import reverse # type: ignore
-from django.contrib.auth import logout # type: ignore
+from django.contrib.auth import logout, login, authenticate # type: ignore
+from django.shortcuts import render # type: ignore
+from django.contrib.auth.forms import UserCreationForm # type: ignore
 
 def logout_view(request):
-    """Faz um Logout doo usuário"""
+    """Faz um Logout doo usuário."""
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    """Cadastro de um novo usuário."""
+    if request.method != 'POST':
+        # Exibe o formulario em branco
+        form = UserCreationForm()
+
+    else:
+        #Processa o formulário preenchido
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            #Faz login do usuário e o redireciona para a página inicial
+            authenticated_user = authenticate(username=new_user.username, password= request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+        
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
